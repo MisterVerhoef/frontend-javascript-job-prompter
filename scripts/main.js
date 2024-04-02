6e549a9187032283ca4c21b0124f908a5a283ce7
MisterVerhoef <frankverhoef@outlook.com>
2024-04-02 16:21:08 +0200

    opdrachten 1 af

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,6 @@ const departments = {
 }
 
 console.log(departments);
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees +  " medewerkers")
+console.log(" Marketing is een leuke afdeling om te werken. " + departments.marketing.description )
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description )
